a0a2cd84384bbf2a827646a73be2ec3451ae4c0b
joelkariyalil1 <joeltc071@gmail.com>
2025-04-20 22:42:23 +0100

    MOD: Changing the Image Size

--- scripts/plot_views.py
+++ scripts/plot_views.py
@@ -4,6 +4,24 @@ import pandas as pd
 df = pd.read_csv("assets/views.csv")
 df["date"] = pd.to_datetime(df["timestamp"])
 df = df.set_index("date")
-df.plot(figsize=(8, 3), linewidth=2)
+
+# Plot with blue line and white text
+ax = df.plot(
+    figsize=(8, 3),
+    linewidth=2.5,
+    color="#00BFFF",  # Bright blue (you can tweak the hex)
+)
+
+# Make all text white
+ax.tick_params(axis='both', which='major', colors='white')
+ax.yaxis.label.set_color('white')
+ax.xaxis.label.set_color('white')
+ax.title.set_color('white')
+
+# Hide borders
+for spine in ax.spines.values():
+    spine.set_visible(False)
+
+# Save to transparent image
 plt.savefig("assets/views.png", transparent=True, bbox_inches="tight", pad_inches=0.1)
 plt.close()
