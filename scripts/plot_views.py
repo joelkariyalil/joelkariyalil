bb73e22f24ce9195bc22c8a227b9f5d2a79266c5
Joel Thomas Chacko <joeltc071@gmail.com>
2025-06-02 23:03:10 +0100

    MOD: Updating it to basically include the month name and the year name in the plot for better UX

--- scripts/plot_views.py
+++ scripts/plot_views.py
@@ -1,16 +1,24 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 
+# Load and prepare data
 df = pd.read_csv("assets/views.csv")
 df["date"] = pd.to_datetime(df["timestamp"])
 df = df.set_index("date")
 
+# Plot
 ax = df.plot(
     figsize=(6, 3),
     linewidth=2.5,
     color="#00BFFF",
 )
 
+# Format x-axis with month and year
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right")
+
+# Aesthetic settings
 ax.tick_params(axis='both', which='major', colors='white')
 ax.yaxis.label.set_color('white')
 ax.xaxis.label.set_color('white')
@@ -19,5 +27,6 @@ ax.title.set_color('white')
 for spine in ax.spines.values():
     spine.set_visible(False)
 
+# Save the plot
 plt.savefig("assets/views.png", transparent=True, bbox_inches="tight", pad_inches=0.1)
 plt.close()
